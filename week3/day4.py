2f73c4aa9fcd7e5f25a58f23fa03e9864484f02d
Saima223 <siddiqui22062004@gmail.com>
2026-04-30 20:00:55 +0530

    file handling

---
+++ week3/day4.py
@@ -0,0 +1,20 @@
+import json
+
+# write file
+with open("test.txt", "w", encoding="utf-8") as f:
+    f.write("Hello 😊")
+
+# read file
+with open("test.txt", "r", encoding="utf-8") as f:
+    print(f.read())
+
+# JSON write
+data = {"name": "Saima", "age": 60}
+
+with open("data.json", "w") as f:
+    json.dump(data, f)
+
+# JSON read
+with open("data.json", "r") as f:
+    data = json.load(f)
+    print(data)
